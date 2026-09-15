3370a1b3d7c900c400858bb706ce8049e6dd9af0
Franciszek Drągowski <f.dragowski@student.uw.edu.pl>
2026-09-15 21:58:09 +0200

    Updated GNU License

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,20 @@
+/*
+  * Copyright (c) 2026 [Franciszek Jan Drągowski]
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program. If not, see <https://www.gnu.org/licenses/>.
+  */
+
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
@@ -37,7 +54,7 @@ int main(int argc, char* argv[]){
 
     Simulation sim(tStepInit, to2PN,
                    /*m1*/1.4414, /*m2*/1.3867,
-                   /*r0*/746600.0, /*v0x*/0.0, /*v0y*/901.6,
+                   /*r0*/746600.0, /*v0x*/0.0, /*v0y*/910.0,
                    absTol, relTol);
 
     // Nagłówek (wiersz 1 to ludzki opis; wiersz 2 to wartości parametrów,
