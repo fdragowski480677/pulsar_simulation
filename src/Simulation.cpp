33a7c27abdc967bf88b3c846bb81370ab49a8608
frantuxed <kalilinuxhackertux@gmail.com>
2026-09-16 15:57:47 +0200

    Updated License again

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,5 +1,5 @@
 /*
-  * Copyright (c) 2026 [Franciszek Jan Drągowski]
+  * Copyright (c) 2026 Franciszek Jan Drągowski, Paweł Sajdak
   *
   * This program is free software: you can redistribute it and/or modify
   * it under the terms of the GNU General Public License as published by
